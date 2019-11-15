60aa4bb9114a02636a943b5869a7898fa3637089
Wing <chooxuanwing@gmail.com>
2019-11-15 22:42:40 +0000

    Initial Commit

---
+++ imagePro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  imagePro
+//
+//  Created by Choo Xuan Wing on 15/11/2019.
+//  Copyright © 2019 Choo Xuan Wing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
