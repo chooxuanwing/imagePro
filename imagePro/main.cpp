a495ab6ed79e94fa056cb784f945be648ec1f7b3
Wing <chooxuanwing@gmail.com>
2019-11-22 03:21:57 +0000

    5

--- imagePro/main.cpp
+++ imagePro/main.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 class file{			// initiate class to store sections of png
 public:
+//	uint32_t test,rawCode;
 	string rawCode;
 	string fileName;
 	
@@ -26,40 +27,40 @@ public:
 class file file;
 
 void openSortFile(string fileName){
-	
-	
-	char sign[8];
+
+
+	unsigned char sign[8], buffer[512];
 	string signature,temp;
-//	uint32_t magic =0;
-	
+
 	ifstream fopen(fileName, std::ios::binary);		// rb is read in binary(hex)
-	
+
 	if (fopen.fail()){
 		cout << "File not found"<< endl;
 		EXIT_FAILURE;			// Quits programme if file not found
 		}
+	
 	else{
+//		fopen.read((unsigned char)&buffer[0], sizeof(buffer));
 		while(getline (fopen, temp)){
 			file.rawCode.append(temp);
 		}
-		cout << file.rawCode;
-//		fread (&magic,sizeof(uint32_t),1,fopen);
-		
-		
-		if(sign[0]==0x89 && sign[1]==0x04 && sign[2]==0x4E &&
-				sign[3]==0x47 && sign[4]==0x0D && sign[5]==0x0A &&
-				sign[6]==0x1A && sign[7]==0x0A){
+		cout << file.rawCode.at(1);
+		int num = std::stoi(file.rawCode, 0, 16);  // trying to convert string to hex
+
+
+		if(file.rawCode.at(0)==0x89 && file.rawCode.at(1)==0x04 && file.rawCode.at(2)==0x4E && file.rawCode.at(3)==0x47 && file.rawCode.at(4)==0x0D && file.rawCode.at(5)==0x0A &&
+			file.rawCode.at(6)==0x1A && file.rawCode.at(7)==0x0A){
 			 /* must have signature of file in
 			 hex is 0x89 0x50 0x4E 0x47*/
 			cout << "Success"<< endl;
-			
+
 		}
 		else{
 			cout << "Signature invalid for PNG" << endl;
 			EXIT_FAILURE;
 		}
 	}
-	
+
 }
 
 int main()
@@ -68,6 +69,8 @@ int main()
 	cout << "Image Processing Software\n\nSpecify the name of the PNG file that you would like to process.\n>";
 //	cin >> fileName;
 	fileName="brainbow.png";
+	
+	
 	openSortFile(fileName);
 }
 
