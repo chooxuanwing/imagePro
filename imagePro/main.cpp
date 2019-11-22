84a4640c18f4b04e7097119a814077223fc23e31
Wing <chooxuanwing@gmail.com>
2019-11-22 02:07:33 +0000

    4

--- imagePro/main.cpp
+++ imagePro/main.cpp
@@ -17,40 +17,57 @@ using namespace std;
 
 class file{			// initiate class to store sections of png
 public:
+	string rawCode;
 	string fileName;
 	
 };
 
-//struct pngFile{
-//	char *dest;
-//	long *destlen;
-//	char *source;
-//	long *sourcelen;
-//};
 
-//struct pngFile pngFile;
 class file file;
 
-void openSortFile(){		// use fread to read chunks later
+void openSortFile(string fileName){
 	
 	char sign[8];
+	string signature,temp;
+	uint32_t magic =0;
 	
-	FILE* bFile;
-	bFile = fopen("file.fileName", "rb");		// rb is read in binary
+	ifstream fopen(fileName, std::ios::binary);		// rb is read in binary(hex)
+	int a;
 	
-	if (bFile == NULL){
+	if (fopen.fail()){
 		cout << "File not found"<< endl;
 		EXIT_FAILURE;			// Quits programme if file not found
 		}
-	
-	
+	else{
+		while(getline (fopen, temp)){
+			file.rawCode.append(temp);
+		}
+		cout << file.rawCode;
+//		fread (&magic,sizeof(uint32_t),1,fopen);
+		
+		
+		if(sign[0]==0x89 && sign[1]==0x04 && sign[2]==0x4E &&
+				sign[3]==0x47 && sign[4]==0x0D && sign[5]==0x0A &&
+				sign[6]==0x1A && sign[7]==0x0A){
+			 /* must have signature of file in
+			 hex is 0x89 0x50 0x4E 0x47*/
+			cout << "Success"<< endl;
+			
+		}
+		else{
+			cout << "Signature invalid for PNG" << endl;
+			EXIT_FAILURE;
+		}
+	}
 	
 }
 
 int main()
 {
+	string fileName;
 	cout << "Image Processing Software\n\nSpecify the name of the PNG file that you would like to process.\n>";
-	cin >> file.fileName;
-	openSortFile();
+//	cin >> fileName;
+	fileName="brainbow.png";
+	openSortFile(fileName);
 }
 
