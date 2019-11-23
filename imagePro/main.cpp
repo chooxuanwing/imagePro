cac535501069f0582fcf2d562e762c7cc30e3b59
Wing <chooxuanwing@gmail.com>
2019-11-23 17:19:05 +0000

    new ver, get line removes stuff thus now its stringstream

--- imagePro/main.cpp
+++ imagePro/main.cpp
@@ -20,6 +20,7 @@ class file{			// initiate class to store sections of png
 public:
 //	uint32_t test,rawCode;
 	vector <unsigned char> rawCode;
+	vector <unsigned int> rawDec;
 	string fileName;
 	
 };
@@ -29,45 +30,48 @@ class file file;
 
 void openSortFile(string fileName){
 
-
-	unsigned char sign[8], buffer[512];
 	string signature,temp;
 
 	ifstream fopen(fileName,std::ios::binary);		// rb is read in binary(hex)
 	
-
 	if (fopen.fail()){
 		cout << "File not found"<< endl;
 		EXIT_FAILURE;			// Quits programme if file not found
 		}
 	
 	else{
-		while(!fopen.eof()){
-			getline(fopen,temp);
-			signature.append(temp);
-		}
-		
+		ostringstream ss;							// Use string stream instead of
+		ss<<fopen.rdbuf();						// getline because getline ignores
+		signature=ss.str();						// whitespace and stuff like 0x1a
+		stringstream line(signature);
+			
+		//	make ss a buffer, rdbuf reads fopen char by char and puts into ss.
+		//	.str func makes ss a string and keeps it in signature
+		//	stringstream primes signature so it can be streamed for processing as a string
+			 
 		for (int i=0;i<signature.length();i++){
 			file.rawCode.push_back(signature.at(i));
+			file.rawDec.push_back(signature.at(i));
+			signature.at(i) >> std::ios::dec >> file.rawDec.at(i);
 		}
-		for (int i=0;i<7;i++)
-			cout << file.rawCode.at(i);
 		
-		if(file.rawCode.at(0)==0x89 && file.rawCode.at(1)==0x50 &&
-		   file.rawCode.at(2)==0x4e && file.rawCode.at(3)==0x47 &&
-		   file.rawCode.at(4)==0x0d && file.rawCode.at(5)==0x0a &&
-		   file.rawCode.at(6)==0x1a && file.rawCode.at(7)==0x0a){
-			 /* must have signature of file in
-			 hex is 0x89 0x50 0x4E 0x47*/
-			cout << "Success"<< endl;
-
+		
+		if(file.rawCode.at(0)==137 && file.rawCode.at(1)==80 &&
+			file.rawCode.at(2)==78 && file.rawCode.at(3)==71 &&
+			file.rawCode.at(4)==13 && file.rawCode.at(5)==10 &&
+			file.rawCode.at(6)==26 && file.rawCode.at(7)==10){
+			
+			// must have signature of file in
+			// decimal, 137 80 78 71 13 10 26 10 for all PNG
+			
+			cout << "Signature Valid"<< endl;
 		}
+
 		else{
 			cout << "Signature invalid for PNG" << endl;
 			EXIT_FAILURE;
 		}
 	}
-
 }
 
 int main()
